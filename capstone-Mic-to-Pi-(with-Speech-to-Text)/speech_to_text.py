87b2502dfba96991d9d95cdafa4d17322b933624
Jack-J-Li <junjieli@ucsb.edu>
2026-04-21 15:01:23 -0700

    Model selection for speech_to_text files are updated
    Now you can use new installation file and test file to test performance (more performance test is needed now for smaller models

--- capstone-Mic-to-Pi-(with-Speech-to-Text)/speech_to_text.py
+++ capstone-Mic-to-Pi-(with-Speech-to-Text)/speech_to_text.py
@@ -4,15 +4,45 @@ from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 
 # Force offline setting (enable it if you want)
 import os
+# ==========================================================
+# Offline Control Code
+# ==========================================================
 os.environ["HF_HUB_OFFLINE"] = "1"
 os.environ["HF_HUB_DISABLE_TELEMETRY"] = "1"
+# ==========================================================
+
+
+# ==========================================================
+# Model Selection
+# 0 = tiny.en
+# 1 = base.en
+# 2 = large-v3
+
+downloadSize = 0
+# Audio file input
+inputAudio = ("your-audio-file")
+# ==========================================================
+
+model_map = {
+    0: ("tiny.en", "./whisper-tiny.en"),
+    1: ("base.en", "./whisper-base.en"),
+    2: ("large-v3", "./whisper-large-v3"),
+}
+
+if downloadSize not in model_map:
+    raise ValueError("Invalid downloadSize. Use 0 (tiny.en), 1 (base.en), or 2 (large-v3).")
+
+model_name, model_path = model_map[downloadSize]
+# Check whether model is installed locally
+if not os.path.isdir(model_path):
+    raise FileNotFoundError(
+        f"the model {model_name} is not installed yet, please install before use."
+    )
 
 
 # Model Setup
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
-
-model_path = "./whisper-large-v3-turbo"   # local folder
 
 model = AutoModelForSpeechSeq2Seq.from_pretrained(
     model_path, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True, local_files_only=True,
@@ -30,8 +60,6 @@ pipe = pipeline(
     device=device,
 )
 
-# USER Test Input
-inputAudio = ("your-audio-file")
 
 result = pipe(
     inputAudio,
